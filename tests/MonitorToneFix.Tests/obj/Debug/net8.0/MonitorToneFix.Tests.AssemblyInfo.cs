03ff8e33765065c9be612cd8b7a1ba456853f925
eltonpaulo <eltonpaulo.agro@gmail.com>
2026-09-14 10:14:27 -0300

    sync automatico [Note-Linux-Elton] 2026-09-14 10:14:27

--- tests/MonitorToneFix.Tests/obj/Debug/net8.0/MonitorToneFix.Tests.AssemblyInfo.cs
+++ tests/MonitorToneFix.Tests/obj/Debug/net8.0/MonitorToneFix.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonitorToneFix.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ff4a4208fe026fb717f63c1a8c61782bbf0032")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677c1a05d3bf36f1823fe5fbfd8694b3f1ff10cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonitorToneFix.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonitorToneFix.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
